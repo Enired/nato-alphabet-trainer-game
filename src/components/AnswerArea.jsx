d720b5150a86955f0a1286f6b1ed2c22c4393d98
Derine Banis <d.banis.13@gmail.com>
2022-08-25 11:22:57 -0700

    Add text field

--- src/components/AnswerArea.jsx
+++ src/components/AnswerArea.jsx
@@ -1,8 +1,13 @@
 export const AnswerArea = (props) => {
   return (
     <div id="answer-area">
-      <button className="btn" id="get-new-letter">New Letter</button>
-      <button className="btn" id="submit-answer">Submit</button>
+      <div className="text-area">
+        <input type="text" name="answer" id="answer" />
+      </div>
+      <div className="answer-area-buttons">
+        <button className="btn" id="get-new-letter">New Letter</button>
+        <button className="btn" id="submit-answer">Submit</button>
+      </div>
     </div>
   )
 }
