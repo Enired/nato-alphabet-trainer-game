c79724a312232030cfb0524193b834d628a807cb
Derine Banis <d.banis.13@gmail.com>
2022-08-25 11:16:14 -0700

    Initial set-up for answer area with buttons

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 //Importing Components
 import { LetterDisplay } from './components/LetterDisplay';
+import { AnswerArea } from './components/AnswerArea';
 function App() {
   return (
     <div className="App">
       <LetterDisplay/>
+      <AnswerArea/>
     </div>
   );
 }
---
+++ src/components/AnswerArea.jsx
@@ -0,0 +1,8 @@
+export const AnswerArea = (props) => {
+  return (
+    <div id="answer-area">
+      <button className="btn" id="get-new-letter">New Letter</button>
+      <button className="btn" id="submit-answer">Submit</button>
+    </div>
+  )
+}
