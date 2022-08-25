d51feb3b4a942c050b618f2a1b3995a198c7fc75
Derine Banis <d.banis.13@gmail.com>
2022-08-25 10:52:55 -0700

    Create helper function and stylesheet

--- src/components/LetterDisplay.jsx
+++ src/components/LetterDisplay.jsx
@@ -1,13 +1,20 @@
+//Import Styles
+import '../styles/LetterDisplay.scss'
+
 export const LetterDisplay = (props) => {
   
-  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
-  const alphabet = alpha.map((x) => String.fromCharCode(x));
-  const selectedLetter = alphabet[Math.floor(Math.random() * 26)]
+  // Return a random letter for user to guess the nato version for.
+  const getRandomLetter = () => {
+    const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+    const alphabet = alpha.map((x) => String.fromCharCode(x));
+    const selectedLetter = alphabet[Math.floor(Math.random() * 26)];
+    return selectedLetter;
+  }
 
 
   return (
     <div id="letter-display">
-      {selectedLetter}
+      {getRandomLetter()}
     </div>
   ) 
 }
