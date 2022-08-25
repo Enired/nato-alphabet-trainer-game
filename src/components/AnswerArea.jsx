1810001188120c9c00843e8d12b4d8ebd31e8ca3
Derine Banis <d.banis.13@gmail.com>
2022-08-25 11:54:32 -0700

    Code cleanup

--- src/components/AnswerArea.jsx
+++ src/components/AnswerArea.jsx
@@ -4,13 +4,39 @@ import "../styles/AnswerArea.scss"
 export const AnswerArea = (props) => {
   return (
     <div id="answer-area">
+      
       <div className="text-area">
-        <input type="text" name="answer" id="answer" maxLength={10}/>
+
+        <input 
+        type="text" 
+        name="answer" 
+        id="answer" 
+        maxLength={10}
+        />
+
       </div>
       <div className="answer-area-buttons">
-        <button className="btn" id="get-new-letter">New Letter</button>
-        <button className="btn" id="submit-answer">Submit</button>
-        <button className="btn" id="clear-answer">Clear</button>
+        <button 
+        className="btn" 
+        id="get-new-letter"
+        >
+          New Letter
+        </button>
+
+        <button 
+        className="btn" 
+        id="submit-answer"
+        >
+          Submit
+        </button>
+
+        <button 
+        className="btn" 
+        id="clear-answer"
+        >
+          Clear
+        </button>
+
       </div>
     </div>
   )
